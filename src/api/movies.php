340cb8ff30084446304347f6fa2b23f2dc474f6e
Ivan <ivan21039@gmail.com>
2020-07-02 19:06:42 +0100

    Fixing vulnerable version of websockets-extensions and php bug in movies.php

--- src/api/movies.php
+++ src/api/movies.php
@@ -10,7 +10,7 @@ function proxyRequest($query) {
     $dotenv->load();
 
     $client = new GuzzleHttp\Client(['base_uri' => 'http://www.omdbapi.com']);
-    $request = new GuzzleHttp\Psr7\Request('GET', "?apikey=${$_ENV['OMDB_KEY']}&s=".urlencode($query));
+    $request = new GuzzleHttp\Psr7\Request('GET', "?apikey={$_ENV['OMDB_KEY']}&s=".urlencode($query));
     $response = $client->send($request);
     return $response->getBody();
 }
